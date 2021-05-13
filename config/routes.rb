e6082969b30ad7c3e2844ab3aa3708d50274c8cb
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-05-13 12:09:51 -0300

    updating stripes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root 'teddies#index'
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create] do
+      resources :payments, only: :new
+  end
+
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# PAREI AQUI
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Teddy.destroy_all if Rails.env.development?
+Category.destroy_all if Rails.env.development?
+
+puts 'Creating categories...'
+geek = Category.create!(name: 'geek')
+kids = Category.create!(name: 'kids')
+
+puts 'Creating teddies...'
+Teddy.create!(price: 10 ,sku: 'original-teddy-bear', name: 'Teddy bear', category: kids,
+photo_url: 'https://images.unsplash.com/photo-1620751992909-d0eaf22f61b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=360&q=80')
+
+Teddy.create!(price_cents: 958 ,sku: 'jean-mini', name: 'Jean-Michel - Le Wagon', category: geek,
+photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(price_cents: 2050 ,sku: 'octocat', name: 'Octacat - GitHub', category: geek,
+photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+puts 'finished!'
